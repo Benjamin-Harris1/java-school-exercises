a9fb09fec31670c764dabb04cfdd0eee6ca3c78f
Benjamin-Harris1 <beha1503@gmail.com>
2024-02-01 11:51:23 +0100

    Strings

--- Strings.java
+++ Strings.java
@@ -7,6 +7,8 @@ public class Strings{
         System.out.println("Fornavn: " + nameParts[1]);
         System.out.println("Mellemnavn: " + nameParts[2]);
         System.out.println("Efternavn: " + nameParts[0]);
+
+        getFullName(nameParts);
     }
 
     public String[] getNameParts(){
@@ -25,6 +27,22 @@ public class Strings{
         }
 
         return new String[] {efternavn, fornavn, mellemnavn};
+    }
+
+    public void getFullName(String lastName, String firstName, String middleName){
+        if (middleName == null) {
+            System.out.println(firstName + " " + lastName);
+        } else {
+            System.out.println(firstName + " " + middleName + " " + lastName);
+        }
+    }
+
+    public void getFullName(String lastName, String firstName){
+        getFullName("Harris", "Benjamin", null);
+    }
+
+    public void getFullName(String[] nameParts){
+        getFullName(nameParts[0], nameParts[1], nameParts[2]);
     }
 
     public static void main(String[] args){
